76daa1672ee152732d374b48ecd5ce944f10c421
Yash Balotiya <balotiyash@gmail.com>
2024-05-25 12:40:44 +0530

    Added dashboard button on navbar, modularised navbar code into different html and included into index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!-- 
+    File: index.php
+    Author: Yash Balotiya
+    Description: This file contains all the HTML5 code of the home/index page.
+    Created on: 24 May 2024
+    Last Modified: 24 May 2024
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- TODO -->
+    <meta name="description" content="Free Web tutorials">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="author" content="Yash Balotiya">
+    <meta name="application-name" content="">
+
+    <!-- Tab Data -->
+    <title>Taxpecharcha</title>
+    <link rel="icon" type="image/x-icon" href="asset/vectors/Rupees.svg">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="shared/style/buttonStyle1.css">
+</head>
+<body>
+    <header>
+        <?php include "shared/view/navBar.html" ?>
+    </header>
+</body>
+</html>
